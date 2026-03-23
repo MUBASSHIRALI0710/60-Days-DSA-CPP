aefbda9e39612024b2cef773f451745a8385eb67
MUBASSHIRALI0710 <mubasshirali0710@gmail.com>
2026-03-23 21:29:07 +0530

    Day01

---
+++ Day01/two_sum.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int a,b;
+  
+  cout << "Enter first Numbers: ";
+  cin >> a;
+
+  cout << "Enter second Numbers: ";
+  cin >> b;
+
+  int sum = a+b;
+  cout <<"Sum = "<< sum << endl;
+  return 0;
+}
