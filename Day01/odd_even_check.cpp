32f91eb0a50946f69244995606ec5291cf10a982
MUBASSHIRALI0710 <mubasshirali0710@gmail.com>
2026-03-24 21:44:39 +0530

    Odd_Even_Check

---
+++ Day01/odd_even_check.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+ 
+int main() {
+ int a;
+ cout <<"Enter Number : ";
+ cin >> a ;
+ if (a % 2==0)
+ {
+  /* code */
+  cout << a <<" is even number"<<endl;
+ }
+ else
+ {
+  /* code */
+  cout << a <<" is odd number"<<endl;
+ }
+  return 0;
+}
