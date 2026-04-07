03273eb26af6e1bcdbef1c637b22b9baac69ed2c
MUBASSHIRALI0710 <mubasshirali0710@gmail.com>
2026-04-07 19:27:07 +0530

    Day06: code practice problems 03 [Maximum Element]

---
+++ Day06/code/program03.cpp
@@ -0,0 +1,21 @@
+// Maximum Element
+// Find the largest element in the array.
+
+#include <iostream>
+using namespace std;
+int main()
+{
+
+  int arr[] = {4, 6, -2, 11, 9};
+  int max = 0;
+  for (int i = 0; i < 5; i++)
+  {
+    if (max < arr[i])
+    {
+      max = arr[i];
+    }
+    }
+  cout << max;
+
+  return 0;
+}
