f93bba2f58fee63879f04a92cdf8cf2319cba8e6
MUBASSHIRALI0710 <mubasshirali0710@gmail.com>
2026-04-14 18:44:54 +0530

    Day09: 1.LeetCode 704 – Binary Search

--- Day09/code/program01.cpp
+++ Day09/code/program01.cpp
@@ -1,13 +1,11 @@
 // 1. LeetCode 704 - Binary Search
 
-
-
 #include <iostream>
 using namespace std;
 int main()
 {
   int arr[] = {-1, 0, 3, 5, 9, 12};
-  int target = 9;
+  int target = 3;
 
   int n = 6;
   int mid, start = 0, end = n - 1;
@@ -16,13 +14,16 @@ int main()
     mid = start + (end - start) / 2;
 
     if (arr[mid] == target)
-      break;
+    {
+      cout <<"Found at index : " << mid;
+      return 0;
+    }
     else if (arr[mid] < target)
       start = mid + 1;
     else
       end = mid - 1;
   }
-  cout << mid;
-  
+  cout << "not found";
+
   return 0;
 }
